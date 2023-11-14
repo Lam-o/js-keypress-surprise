258b38e2d3cdd2efe50e78d47aff9d86472065d8
lam <i.mailed.lam@gmail.com>
2023-11-14 10:39:59 +0200

    PRess key and see

---
+++ js/script.js
@@ -0,0 +1,30 @@
+var box1 = document.querySelector(".box-1");
+var box2 = document.querySelector(".box-2");
+var box3 = document.querySelector(".box-3");
+var button = document.querySelector("button");
+//add event listener
+
+document.addEventListener("keydown", function (e) {
+  //console.log(e);
+  //console.log(e.key);
+  var key = e.key;
+
+  if (key === "1") {
+    box1.innerText = "💀";
+  } else if (key === "2") {
+    box2.innerText = "👻";
+  } else if (key === "3") {
+    box3.innerText = "👽";
+  }
+});
+
+button.addEventListener("click", function () {
+  box1.innerText = "1";
+  box2.innerText = "2";
+  box3.innerText = "3";
+});
+
+// emojis to use:
+("💮");
+("🚀");
+("😸");
